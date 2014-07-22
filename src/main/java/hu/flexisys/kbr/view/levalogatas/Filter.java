e7a59d3764a63d9871cea01d8eaf924089207332
Fr?ny?, P?ter <franyo.peter@gmail.com>
2014-07-22 17:08:33 +0200

    sz?k?t?s poc

---
+++ src/main/java/hu/flexisys/kbr/view/levalogatas/Filter.java
@@ -0,0 +1,41 @@
+package hu.flexisys.kbr.view.levalogatas;
+
+import java.util.HashMap;
+
+/**
+ * Created by Peter on 2014.07.22..
+ */
+public class Filter {
+
+    public static final String MAR_ELLETT = "MAR_ELLETT";
+    public static final String NEM_BIRALT = "NEM_BIRALT";
+    public static final String ITV = "ITV";
+    public static final String ELLES_SORSZAMAI = "ELLES_SORSZAMAI";
+    public static final String HU = "HU";
+    public static final String ENAR = "ENAR";
+    public static final String UTOLSO_ELLES_TOL = "UTOLSO_ELLES_TOL";
+    public static final String UTOLSO_ELLES_IG = "UTOLSO_ELLES_IG";
+    public static final String SZULETES_TOL = "SZULETES_TOL";
+    public static final String SZULETES_IG = "SZULETES_IG";
+    public static final String KONSTRUKCIOS_TOL = "KONSTRUKCIOS_TOL";
+    public static final String KONSTRUKCIOS_IG = "KONSTRUKCIOS_IG";
+
+    public HashMap<String, Object> filterMap;
+
+    public Filter() {
+        filterMap = new HashMap<String, Object>();
+    }
+
+    public Object get(String key) {
+        return filterMap.get(key);
+    }
+
+    public void put(String key, Object value) {
+        filterMap.put(key, value);
+    }
+
+    public void clear(){
+        filterMap.clear();
+    }
+
+}
--- src/main/java/hu/flexisys/kbr/view/levalogatas/LevalogatasActivity.java
+++ src/main/java/hu/flexisys/kbr/view/levalogatas/LevalogatasActivity.java
@@ -1,14 +1,13 @@
 package hu.flexisys.kbr.view.levalogatas;
 
 import android.os.Bundle;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
 import hu.flexisys.kbr.R;
 import hu.flexisys.kbr.model.Biralat;
 import hu.flexisys.kbr.model.Egyed;
@@ -29,8 +28,10 @@ public class LevalogatasActivity extends KbrActivity {
     private long[] selectedTenazArray;
     private List<Egyed> egyedList;
     private Integer selectedEgyedCounter;
-
     private LevalogatasListViewAdapter adapter;
+
+    private SlidingPaneLayout pane;
+    private Filter filter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,8 +41,29 @@ public class LevalogatasActivity extends KbrActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowTitleEnabled(false);
 
+        filter = new Filter();
+
         selectedTenazArray = getIntent().getExtras().getLongArray(BiralatTenyeszetActivity.EXTRAKEY_SELECTEDTENAZLIST);
+        egyedList = new ArrayList<Egyed>();
         reloadData();
+
+        pane = (SlidingPaneLayout) findViewById(R.id.sp);
+        pane.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onPanelOpened(View panel) {
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onPanelClosed(View panel) {
+                invalidateOptionsMenu();
+            }
+        });
 
         TextView telep = (TextView) findViewById(R.id.lev_telep_name);
         List<Tenyeszet> tenyeszetList = app.getTenyeszetListByTENAZArray(selectedTenazArray);
@@ -71,8 +93,48 @@ public class LevalogatasActivity extends KbrActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        int resId = pane.isOpen() ? R.menu.menu_activity_levalogatas_szukites : R.menu.menu_activity_levalogatas;
+        inflater.inflate(resId, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mentes_kilepes:
+                mentes_kilepes();
+                return true;
+            case R.id.mentes:
+                mentes();
+                return true;
+            case R.id.torles:
+                torles();
+                return true;
+            case R.id.levalogatas_torles:
+                levalogatas_torles();
+                return true;
+            case R.id.szukit:
+                szukit();
+                return true;
+            case R.id.urit:
+                urit();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void reloadData() {
-        egyedList = app.getEgyedListByTENAZArray(selectedTenazArray);
+        egyedList.clear();
+        List<Egyed> rawList = app.getEgyedListByTENAZArray(selectedTenazArray);
+        for (Egyed egyed : rawList) {
+            if (applyFilter(egyed)) {
+                egyedList.add(egyed);
+            }
+        }
         selectedEgyedCounter = 0;
 
         if (egyedList.isEmpty()) {
@@ -101,41 +163,47 @@ public class LevalogatasActivity extends KbrActivity {
         }
     }
 
+    private boolean applyFilter(Egyed egyed) {
+        String esFilter = (String) filter.get(Filter.ELLES_SORSZAMAI);
+        if (esFilter != null && !esFilter.isEmpty()) {
+            String[] esArray = esFilter.split(",");
+            Boolean cool = false;
+            for (String es : esArray) {
+                if (Integer.valueOf(es).equals(egyed.getELLSO())) {
+                    cool = true;
+                    break;
+                }
+            }
+            if (!cool) {
+                return false;
+            }
+        }
+
+        String enar = (String) filter.get(Filter.ENAR);
+        if (enar != null && !enar.isEmpty()) {
+            if (egyed.getAZONO() == null || !enar.equals(String.valueOf(egyed.getAZONO()))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private void updateCounters() {
         TextView counterView = (TextView) findViewById(R.id.lev_selected_egyed_counter);
         counterView.setText(String.valueOf(selectedEgyedCounter));
         counterView = (TextView) findViewById(R.id.lev_egyed_counter);
         counterView.setText(String.valueOf(egyedList.size()));
+
+        counterView = (TextView) findViewById(R.id.lev_szuk_selected_egyed_counter);
+        counterView.setText(String.valueOf(selectedEgyedCounter));
+        counterView = (TextView) findViewById(R.id.lev_szuk_egyed_counter);
+        counterView.setText(String.valueOf(egyedList.size()));
     }
+
+    // LEVÁLOGATÁS
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
     // MENU IN ACTIONBAR
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_activity_levalogatas, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.mentes_kilepes:
-                mentes_kilepes();
-                return true;
-            case R.id.mentes:
-                mentes();
-                return true;
-            case R.id.torles:
-                torles();
-                return true;
-            case R.id.levalogatas_torles:
-                levalogatas_torles();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     public void mentes_kilepes() {
         Log.i(TAG, "saveLevalogatasStarted");
@@ -184,8 +252,49 @@ public class LevalogatasActivity extends KbrActivity {
     }
 
     public void torles() {
+        toast("Not implemented");
     }
 
     public void levalogatas_torles() {
+        toast("Not implemented");
+    }
+
+    // SZŰKÍTÉS
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    public void szukit() {
+        EditText enarET = (EditText) findViewById(R.id.lev_szuk_enar);
+        String enarFilter = enarET.getText().toString();
+        filter.put(Filter.ENAR, enarFilter);
+
+        EditText esET = (EditText) findViewById(R.id.lev_szuk_elles_sorszamai);
+        String esFilter = esET.getText().toString();
+        filter.put(Filter.ELLES_SORSZAMAI, esFilter);
+
+        Log.i(TAG, "szukitLevalogatasStarted");
+        startProgressDialog();
+        EmptyTask task = new EmptyTask(new Executable() {
+            @Override
+            public void execute() {
+                reloadData();
+            }
+        }, new ExecutableFinishedListener() {
+            @Override
+            public void onFinished() {
+                updateCounters();
+                adapter.notifyDataSetChanged();
+                dismissDialog();
+                Log.i(TAG, "szukitLevalogatasEnded");
+            }
+        });
+        task.execute();
+    }
+
+    public void urit() {
+        toast("Not implemented");
+    }
+
+    public void showSzukites(View view) {
+        pane.openPane();
     }
 }
