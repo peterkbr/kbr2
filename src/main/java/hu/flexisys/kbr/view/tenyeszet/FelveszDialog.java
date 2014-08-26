04267050734a798c88b92c40b44a794b39b85906
Fr?ny? P?ter <franyo.peter@gmail.com>
2014-08-26 17:20:07 +0200

    KBR-1 : Teny?szetek kezel?sel : teny?szet felv?tele dialog

--- src/main/java/hu/flexisys/kbr/view/tenyeszet/FelveszDialog.java
+++ src/main/java/hu/flexisys/kbr/view/tenyeszet/FelveszDialog.java
@@ -1,9 +1,11 @@
 package hu.flexisys.kbr.view.tenyeszet;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import hu.flexisys.kbr.R;
@@ -28,6 +30,8 @@ public class FelveszDialog extends KbrDialog {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = super.onCreateView(inflater, container, savedInstanceState);
         azonEdit = (EditText) v.findViewById(R.id.teny_dialog_azon);
+        toggleKeyboard();
+        toggleKeyboard();
 
         Button ok = (Button) v.findViewById(R.id.dialog_felvesz_ok);
         ok.setOnClickListener(new View.OnClickListener() {
@@ -48,12 +52,21 @@ public class FelveszDialog extends KbrDialog {
     }
 
     public void cancel() {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(azonEdit.getWindowToken(), 0);
         dismiss();
     }
 
     public void ok() {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(azonEdit.getWindowToken(), 0);
         listener.onFelvesz(azonEdit.getText().toString());
         dismiss();
+    }
+
+    public void toggleKeyboard() {
+        InputMethodManager imgr = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+        imgr.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 
 }
