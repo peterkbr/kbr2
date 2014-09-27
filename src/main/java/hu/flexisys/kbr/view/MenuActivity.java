7eecdea7bf019a87f0005f9acb94e855264fbcba
Fr?ny? P?ter <franyo.peter@gmail.com>
2014-09-27 13:13:59 +0200

    KBR-114 : Menu a menu-ben

--- src/main/java/hu/flexisys/kbr/view/MenuActivity.java
+++ src/main/java/hu/flexisys/kbr/view/MenuActivity.java
@@ -2,8 +2,9 @@ package hu.flexisys.kbr.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.*;
 import android.widget.TextView;
+import android.widget.Toast;
 import hu.flexisys.kbr.R;
 import hu.flexisys.kbr.view.biralat.BiralatTenyeszetActivity;
 import hu.flexisys.kbr.view.bongeszo.tenyeszet.BongeszoTenyeszetActivity;
@@ -15,8 +16,14 @@ public class MenuActivity extends KbrActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        actionBar.hide();
         setContentView(R.layout.activity_menu);
+
+        View customView = LayoutInflater.from(this).inflate(R.layout.activity_menu_actionbar, null);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setCustomView(customView);
+
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
     }
 
     @Override
@@ -26,6 +33,8 @@ public class MenuActivity extends KbrActivity {
         int counterValue = app.getFeltoltetlenBiralatList().size();
         counter.setText(getString(R.string.menu_biralat_counter, counterValue));
     }
+
+    // APP MENU
 
     public void startTenyeszet(View view) {
         Intent intent = new Intent(this, TenyeszetActivity.class);
@@ -45,6 +54,32 @@ public class MenuActivity extends KbrActivity {
     public void startBiralatbongeszo(View view) {
         Intent intent = new Intent(this, BongeszoTenyeszetActivity.class);
         startActivity(intent);
+    }
+
+    // ACTIONBAR MENU
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.db:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu:
+                Toast.makeText(this, "Not implemented yet!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
