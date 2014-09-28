c9d68a203a50bd8edc43da8da22ba4c656e110e2
Fr?ny? P?ter <franyo.peter@gmail.com>
2014-09-28 17:21:24 +0200

    KBR-86 : view updated

---
+++ src/main/java/hu/flexisys/kbr/view/levalogatas/LeválogatásExportDialog.java
@@ -0,0 +1,65 @@
+package hu.flexisys.kbr.view.levalogatas;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import hu.flexisys.kbr.R;
+import hu.flexisys.kbr.view.KbrDialog;
+
+/**
+ * Created by peter on 25/08/14.
+ */
+public class LeválogatásExportDialog extends KbrDialog {
+
+    private static final String TAG = "KBR2_ExportDialog";
+    private ExportListener listener;
+
+    public static KbrDialog newInstance(ExportListener listener) {
+        LeválogatásExportDialog dialog = new LeválogatásExportDialog();
+        dialog.layoutResId = R.layout.dialog_levalogatas_export;
+        dialog.listener = listener;
+        return dialog;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View v = super.onCreateView(inflater, container, savedInstanceState);
+
+        Button ok = (Button) v.findViewById(R.id.dialog_export_ok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button) {
+                CheckBox pdfCheckBox = (CheckBox) v.findViewById(R.id.dialog_export_checkBox_pdf);
+                Boolean pdf = pdfCheckBox.isChecked();
+                CheckBox csvCheckBox = (CheckBox) v.findViewById(R.id.dialog_export_checkBox_csv);
+                Boolean csv = csvCheckBox.isChecked();
+                if (!pdf && !csv) {
+                    // TODO i18n
+                    Toast.makeText(getActivity(), "Kérem válasszon formátumot!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                listener.onExport(pdf, csv);
+            }
+        });
+
+        Button cancel = (Button) v.findViewById(R.id.dialog_export_cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View button) {
+                dismiss();
+            }
+        });
+
+        return v;
+    }
+
+    public interface ExportListener {
+        public void onExport(boolean pdf, boolean csv);
+    }
+}
