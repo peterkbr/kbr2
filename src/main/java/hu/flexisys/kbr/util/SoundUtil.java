06f381a3ec3d5a68891187d77e5f1f581d492802
Fr?ny? P?ter <franyo.peter@gmail.com>
2014-09-30 18:46:16 +0200

    KBR-90 : Gombnyom?sra pityegjen az alkalmaz?s, most ha gyorsan p?ty?g?nk, akkor kimarad p?r csipog?s

--- src/main/java/hu/flexisys/kbr/util/SoundUtil.java
+++ src/main/java/hu/flexisys/kbr/util/SoundUtil.java
@@ -1,7 +1,8 @@
 package hu.flexisys.kbr.util;
 
 import android.content.Context;
-import android.media.MediaPlayer;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import hu.flexisys.kbr.R;
 
 /**
@@ -9,22 +10,31 @@ import hu.flexisys.kbr.R;
  */
 public class SoundUtil {
 
-    private static MediaPlayer errorPlayer;
-    private static MediaPlayer buttonPlayer;
+    private static Context context;
+    private static SoundPool soundPool;
+    private static int buttonSounID;
+    private static int errorSounID;
 
     public static void initSoundUtil(Context context) {
-        errorPlayer = MediaPlayer.create(context, R.raw.error_beep);
-        errorPlayer.setVolume(0.05f, 0.05f);
-
-        buttonPlayer = MediaPlayer.create(context, R.raw.button_beep);
-        buttonPlayer.setVolume(0.05f, 0.05f);
+        SoundUtil.context = context;
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        buttonSounID = soundPool.load(context, R.raw.button_beep, 1);
+        errorSounID = soundPool.load(context, R.raw.error_beep, 1);
     }
 
     public static void errorBeep() {
-        errorPlayer.start();
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float volume = actualVolume / maxVolume;
+        soundPool.play(errorSounID, volume, volume, 1, 0, 1f);
     }
 
     public static void buttonBeep() {
-        buttonPlayer.start();
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float volume = actualVolume / maxVolume;
+        soundPool.play(buttonSounID, volume, volume, 1, 0, 1f);
     }
 }
